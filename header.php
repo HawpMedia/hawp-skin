d6858519983e0ea2a2c9fc297540db4f0af51f4e
Evan Fraser <evan@hawpmedia.com>
2025-11-25 14:44:57 -0600

    Refactor template-part layout naming to nav/structure scheme

--- header.php
+++ header.php
@@ -3,18 +3,18 @@
  * The child header file.
  */
 
-get_template_part('parts/layout/head');
+get_template_part('parts/structure/head');
 ?>
 
 <header id="header" class="header contain" role="banner">
 
 	<?php echo do_shortcode('[logo class="logo"]'); ?>
 
-	<?php get_template_part('parts/layout/header-menu-buttons'); ?>
+	<?php get_template_part('parts/structure/nav-controls-default'); ?>
 
 </header>
 
-<?php get_template_part('parts/layout/header-menu'); ?>
+<?php get_template_part('parts/structure/nav-primary'); ?>
 
 <main id="main" role="main">
 
